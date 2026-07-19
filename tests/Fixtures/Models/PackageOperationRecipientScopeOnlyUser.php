d51cfe536fcadf76f9fe00afc277be5389460c16
Ben <howdu@users.noreply.github.com>
2026-07-19 12:49:19 +0100

    Release v1.0.10

---
+++ tests/Fixtures/Models/PackageOperationRecipientScopeOnlyUser.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Capell\Admin\Tests\Fixtures\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
+
+final class PackageOperationRecipientScopeOnlyUser extends Authenticatable
+{
+    /** @use HasFactory<Factory<self>> */
+    use HasFactory;
+
+    use HasRoles;
+
+    protected $table = 'users';
+
+    public function guardName(): string
+    {
+        return 'web';
+    }
+
+    /**
+     * @param  Builder<self>  $query
+     * @return Builder<self>
+     */
+    protected function scopeGlobalAdmins(Builder $query): Builder
+    {
+        return $query;
+    }
+}
